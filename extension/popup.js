d2874abe55f7f34e9e83294c772a4d620a090b3d
festring <146055385+festring@users.noreply.github.com>
2023-12-05 05:26:51 +0900

    Delete extension directory

--- extension/popup.js
+++
@@ -1,8 +0,0 @@
-// popup.js
-
-// 저장된 ID를 가져와서 팝업 HTML에 표시
-chrome.storage.sync.get(['extensionId'], function (result) {
-    const extensionId = result.extensionId || 'ID가 없습니다.';
-    document.getElementById('extensionId').textContent = "참가자님의 ID: "+extensionId;
-  });
-  
